3d8c1ae98b92ba3338132c5e99f138345fc493c8
Akshit Abhinav Kujur <143180641+Rndmcoder@users.noreply.github.com>
2025-04-08 14:34:21 +0530

    Add files via upload

---
+++ itinerary/flight.py
@@ -0,0 +1,10 @@
+#flight.py
+class Flight:
+    def __init__(self, start_city, end_city, departure_time, arrival_time, airline, fare, flight_no):
+        self.start_city = start_city
+        self.end_city = end_city
+        self.departure_time = departure_time
+        self.arrival_time = arrival_time
+        self.airline = airline
+        self.fare = fare
+        self.flight_no = flight_no
