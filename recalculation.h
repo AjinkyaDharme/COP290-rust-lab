494505a590a6184e2585c60018ddcc139dcc1948
AJINKYA NARENDRA DHARME <161201363+AjinkyaDharme@users.noreply.github.com>
2025-04-25 14:36:17 +0530

    Delete recalculation.h

--- recalculation.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECALCULATION_H
-#define RECALCULATION_H
-
-#include "spreadsheet.h"
-
-void recalc_topological(Spreadsheet *sheet, short changed_row, short changed_col);
-void recalculate_cell(Spreadsheet *sheet, short row, short col);
-
-#endif
