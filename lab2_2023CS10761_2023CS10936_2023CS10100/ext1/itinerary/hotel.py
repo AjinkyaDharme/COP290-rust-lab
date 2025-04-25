8c6fd13c2e2206bcb0ded9f8e466d31f19904f81
AJINKYA NARENDRA DHARME <161201363+AjinkyaDharme@users.noreply.github.com>
2025-04-25 15:25:58 +0530

    Delete lab2_2023CS10761_2023CS10936_2023CS10100 directory

--- lab2_2023CS10761_2023CS10936_2023CS10100/ext1/itinerary/hotel.py
+++
@@ -1,8 +0,0 @@
-class Hotel:
-    def __init__(self, city, hotel_name, address, checkin, checkout, price):
-        self.city = city
-        self.hotel_name = hotel_name
-        self.address = address
-        self.checkin = checkin
-        self.checkout = checkout
-        self.price = price
